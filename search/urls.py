9acd098832f4af37f81d40cfa95201fd7a46cacc
hermann <laplacefst@gmail.com>
2020-09-02 11:52:47 +0900

    door.html 1

--- search/urls.py
+++ search/urls.py
@@ -5,7 +5,6 @@ app_name = 'search'
 urlpatterns = [
     path('search/', views.search, name='search'),
     path('artist_list/<int:artist_id>', views.artist_list, name='artist_list'),
-    path('video/', views.video, name='video'),
     path('genre/', views.genre, name='genre'),
     path('chart/', views.chart, name='chart'),
 ]
